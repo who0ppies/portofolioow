b4435df05aa57a43136138e8d339d63d854b0821
fikah <fadiyah.s3103@gmail.com>
2025-12-08 08:10:35 +0700

    update footer

--- script.js
+++ script.js
@@ -1,59 +1,24 @@
-document.querySelectorAll('nav a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    const targetId = this.getAttribute("href");
-    const targetElement = document.querySelector(targetId);
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("contactForm");
 
-    if (targetElement) {
-      targetElement.scrollIntoView({
-        behavior: "smooth",
-      });
-    }
-  });
-});
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
 
-const navbar = document.querySelector(".navbar");
+        try {
+            const res = await fetch("https://formspree.io/f/xpwekbny", {
+                method: "POST",
+                body: new FormData(form),
+                headers: { "Accept": "application/json" }
+            });
 
-function toggleStickyNavbar() {
-  if (window.scrollY > 50) {
-    navbar.classList.add("scrolled");
-  } else {
-    navbar.classList.remove("scrolled");
-  }
-}
-
-window.addEventListener("scroll", toggleStickyNavbar);
-
-const projectButtons = document.querySelectorAll(".view-btn");
-
-projectButtons.forEach((button) => {
-  button.addEventListener("click", function (e) {
-    console.log("Navigating to project - " + this.getAttribute("href"));
-  });
-});
-
-const contactForm = document.querySelector(".contact form");
-
-contactForm.addEventListener("submit", function (e) {
-  console.log("Form submitted. Redirecting to Formspree action...");
-});
-
-const skillCards = document.querySelectorAll(".skill-card");
-
-const observerOptions = {
-  threshold: 0.2,
-};
-
-const skillObserver = new IntersectionObserver((entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("fade-in");
-      observer.unobserve(entry.target);
-    }
-  });
-}, observerOptions);
-
-skillCards.forEach((card, index) => {
-  card.style.animationDelay = $(index * 0.1);
-  skillObserver.observe(card);
+            const data = await res.json();
+          if  {
+                form.reset();
+            } else {
+                alert("Error: " + JSON.stringify(data));
+            }
+        } catch (err) {
+            alert("Gagal mengirim.");
+        }
+    });
 });
